bfd47ef3728125e658b9028d93ac244820ce5c35
Ruben Karlsson <neburgordon@gmail.com>
2020-12-11 15:12:12 +0100

    hide secret key

--- poemautomator/settings.py
+++ poemautomator/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ STATIC_DIR = BASE_DIR / "static"
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^&2%9tyu)^)v278hf03l44+!o6dz=w@mv**o)s)y^wdek_r01u'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
